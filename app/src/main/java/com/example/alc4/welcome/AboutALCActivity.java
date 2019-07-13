947847ae48dd093783d23c953be7ac9d3d8cd72c
Joseph Michael <tjoe4u@gmail.com>
2019-07-13 11:36:37 +0100

    adds launcher icon and other activities

---
+++ app/src/main/java/com/example/alc4/welcome/AboutALCActivity.java
@@ -0,0 +1,52 @@
+package com.example.alc4.welcome;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.http.SslError;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutALCActivity extends AppCompatActivity {
+
+    final String URL = "https://andela.com/alc/";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        WebView webView = findViewById(R.id.wvAboutALC);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        final AboutALCActivity context = this;
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+                // for SSLErrorHandler
+                final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage(R.string.notification_error_ssl_cert_invalid);
+                builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.proceed();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.cancel();
+                        finish();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+        webView.loadUrl(URL);
+    }
+
+}
--- app/src/main/java/com/example/alc4/welcome/MainActivity.java
+++ app/src/main/java/com/example/alc4/welcome/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.alc4.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button about;
+    Button profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        about = findViewById(R.id.btAbout);
+        about.setOnClickListener(this);
+
+        profile = findViewById(R.id.btProfile);
+        profile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println("onClick");
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.btAbout:
+                intent = new Intent(this, AboutALCActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btProfile:
+                intent = new Intent(this, MyProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
